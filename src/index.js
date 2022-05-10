95463ae57ac628ec16141345b8f82a8bd2214b24
Solomon Achilles <solohag92@gmail.com>
2022-05-10 15:09:22 +0000

    Render getRockets action once

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/configureStore';
+import { getRockets } from './redux/rockets';
+
+store.dispatch(getRockets());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/pages/Rockets.js
+++ src/pages/Rockets.js
@@ -1,15 +1,9 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getRockets } from '../redux/rockets';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import Rocket from './Rocket';
 
 const Rockets = () => {
   const rockets = useSelector((state) => state.rockets);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getRockets());
-  }, [dispatch]);
 
   return (
     <div className="rockets d-col-flex">

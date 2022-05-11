f0bc57d4d259d0e4dbaeb3f89801064d7f38bfca
Solomon Achilles <solohag92@gmail.com>
2022-05-11 09:09:18 +0000

    Update Rocket's display

--- src/pages/Rocket.js
+++ src/pages/Rocket.js
@@ -19,8 +19,22 @@ const Rocket = (props) => {
       <img className="rocket-image" src={rocket.flickr_images} alt={`${rocket.rocket_name}`} />
       <div className="rocket-details d-col-flex">
         <h2>{rocket.rocket_name}</h2>
-        <p>{rocket.description}</p>
-        <button type="button" onClick={handleClick}>Reserve Rocket</button>
+        {rocket.reserved && rocket.reserved === true
+          ? (
+            <div className="cancel-reservation d-col-flex">
+              <p>
+                <span>Reserved</span>
+                {rocket.description}
+              </p>
+              <button type="button" onClick={handleClick}>Cancel Reservation</button>
+            </div>
+          )
+          : (
+            <div className="reserve-rocket d-col-flex">
+              <p>{rocket.description}</p>
+              <button type="button" onClick={handleClick}>Reserve Rocket</button>
+            </div>
+          )}
       </div>
     </div>
   );

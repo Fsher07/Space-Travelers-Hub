d101c25bf5c3bab0c4ceea2fb7139eca7407bf11
Fsher07 <furkansalimm@gmail.com>
2022-05-12 17:32:26 +0300

    Fix linters

--- src/__tests__/MyProfile.test.js
+++ src/__tests__/MyProfile.test.js
@@ -3,7 +3,6 @@ import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import MyProfile from '../pages/MyProfile';
 import testStore from '../redux/configureStore';
-import Rockets from '../pages/Rockets';
 import MissionsList from '../pages/MissionsList';
 
 const mission = [
@@ -12,17 +11,7 @@ const mission = [
     key: 'special',
     id: 1,
     name: 'defines the satellite',
-    reservation: true,
-  },
-];
-
-const rocket = [
-  {
-    desc: 'Falcon 9 is a two-stage rocket designed and manufactured by SpaceX',
-    key: 'very special',
-    id: 2,
-    name: 'defines the rocket',
-    reservation: true,
+    reservation: false,
   },
 ];
 
@@ -30,11 +19,11 @@ describe('testing the rendering of my profile page', () => {
   it('renders rocket and missions', () => {
     const { container } = render(
       <Provider store={testStore}>
-        <MyProfile />
-          <Rockets key={rocket[0].id} rocket={rocket[0]} />
+        <MyProfile>
           <MissionsList key={mission[0].id} mission={mission[0]} />
+        </MyProfile>
       </Provider>,
-    )
-  })
+    );
+    expect(container).toMatchSnapshot();
+  });
 });
-
